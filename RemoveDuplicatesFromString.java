c1eafe62d21732412496c5dc9b2fa7525f14bb5b
Tejas-1902 <114847977+Tejas-1902@users.noreply.github.com>
2023-09-21 21:17:07 +0530

    Add files via upload

---
+++ RemoveDuplicatesFromString.java
@@ -0,0 +1,84 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class RemoveDuplicatesFromString {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int tc = Integer.parseInt(br.readLine());
+        while (tc-- > 0) {
+            String str = br.readLine().trim();
+
+            String ans = new Solution().removeDuplicates(str);
+            System.out.println(ans);
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+
+class Solution {
+    String //{ Driver Code Starts
+//Initial Template for Java
+
+
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int tc = Integer.parseInt(br.readLine());
+        while (tc-- > 0) {
+            String str = br.readLine().trim();
+
+            String ans = new Solution().removeDuplicates(str);
+            System.out.println(ans);
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+
+class Solution {
+    String removeDuplicates(String str) {
+        // code here
+        String ds="";
+        for(int i=0;i<str.length();i++)
+        {
+            if(ds.indexOf(str.charAt(i))==-1)
+            ds+=str.charAt(i);
+        }
+        return ds;
+    }
+}
+
+(String str) {
+        // code here
+        String ds="";
+        for(int i=0;i<str.length();i++)
+        {
+            if(ds.indexOf(str.charAt(i))==-1)
+            ds+=str.charAt(i);
+        }
+        return ds;
+    }
+}
+
