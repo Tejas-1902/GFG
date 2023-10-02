e0fa6463ea73959cd0d9d10bd6941c95976ecfe4
Tejas-1902 <114847977+Tejas-1902@users.noreply.github.com>
2023-10-02 20:58:27 +0530

    MaximumKNegotiation.java

--- MaximumKNegotiation.java
+++ MaximumKNegotiation.java
@@ -6,63 +6,36 @@ import java.lang.*;
 import java.io.*;
 
 class MaximumKNegotiation {
-	public static void main(String[] args) throws IOException
+	public static void main(String[] args)
 	{
-	        BufferedReader br =
-            new BufferedReader(new InputStreamReader(System.in));
-        int t =
-            Integer.parseInt(br.readLine().trim()); // Inputting the testcases
-        while(t-->0)
-        {
-            StringTokenizer stt = new StringTokenizer(br.readLine());
-            
-            int n = Integer.parseInt(stt.nextToken());
-            int k = Integer.parseInt(stt.nextToken());
-            long a[] = new long[(int)(n)];
-            String inputLine[] = br.readLine().trim().split(" ");
-            for (int i = 0; i < n; i++) {
-                a[i] = Long.parseLong(inputLine[i]);
-            }
-            
-            Solution obj = new Solution();
-            System.out.println(obj.maximizeSum(a, n, k));
-            
-        }
-	}
-}
+	        int[] arr = {5, -2, 5, -4, 5, -12, 5, 5, 5, 20};
+	        int n = arr.length;
+                int k=5;
+	        // Your code goes here  
+	        Arrays.sort(arr);
 
-// } Driver Code Ends
+	         // Negate the smallest K elements
+	         int i = 0;
+	         while (k > 0 && i < n && arr[i] < 0) {
+	             arr[i] = -arr[i];
+	             k--;
+	             i++;
+	         }
 
+	         // If K is still greater than 0 and even, keep negating any element with the smallest absolute value
+	         if (k > 0 && k % 2 != 0) {
+	             Arrays.sort(arr); 
+	             arr[0] = -arr[0];
+	         }
 
-//User function Template for Java
+	         // Calculate the sum of the modified array
+	         long sum = 0;
+	         for (long num : arr) {
+	             sum += num;
+	         }
 
+	         
 
-class Solution {
-
-    public static long maximizeSum(long arr[], int n, int k)
-    {
-        // Your code goes here  
-       Arrays.sort(arr);
-
-        int i = 0;
-        while (k > 0 && i < n && arr[i] < 0) {
-            arr[i] = -arr[i];
-            k--;
-            i++;
-        }
-
-        // If K is still greater than 0 and even, keep negating any element with the smallest absolute value
-        if (k > 0 && k % 2 != 0) {
-            Arrays.sort(arr); // Sort again to ensure we negate the smallest absolute value
-            arr[0] = -arr[0];
-        }
-
-        // Calculate the sum 
-        long sum = 0;
-        for (long num : arr) {
-            sum += num;
-        }
-
-        return sum;
+	       System.out.println(sum);
     }
 }
